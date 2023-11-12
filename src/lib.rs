3e8fd20ba1993d05887d22933c38b8ba4fc3b245
Ivor Wanders <ivor@iwanders.net>
2023-11-12 13:47:42 -0500

    Format.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,1 @@
-
 pub mod pnpoly;
-
-
--- src/pnpoly.rs
+++ src/pnpoly.rs
@@ -1,5 +1,3 @@
-
-
 // https://wrfranklin.org/Research/Short_Notes/pnpoly.html
 
 /*
@@ -14,7 +12,7 @@ int pnpoly(int nvert, float *vertx, float *verty, float testx, float testy)
   int i, j, c = 0;
   for (i = 0, j = nvert-1; i < nvert; j = i++) {
     if ( ((verty[i]>testy) != (verty[j]>testy)) &&
-	 (testx < (vertx[j]-vertx[i]) * (testy-verty[i]) / (verty[j]-verty[i]) + vertx[i]) )
+     (testx < (vertx[j]-vertx[i]) * (testy-verty[i]) / (verty[j]-verty[i]) + vertx[i]) )
        c = !c;
   }
   return c;
@@ -39,13 +37,13 @@ pub fn inside(vertices: &[(f32, f32)], test: &(f32, f32)) -> bool {
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
     // https://github.com/boostorg/geometry/blob/3f5c044abcc813a36d6af83465a9c086f9728a2f/test/strategies/franklin.cpp
 
     #[test]
     fn test_triangle() {
-        let triangle = vec![(0.0, 0.0), (0.0,  4.0), (6.0,  0.0), (0.0, 0.0)];
+        let triangle = vec![(0.0, 0.0), (0.0, 4.0), (6.0, 0.0), (0.0, 0.0)];
         assert_eq!(inside(&triangle, &(1.0, 1.0)), true);
         assert_eq!(inside(&triangle, &(3.0, 3.0)), false);
 
@@ -60,7 +58,7 @@ mod test{
 
     #[test]
     fn test_square() {
-        let square = vec![(0.0, 0.0), (0.0,  2.0), (2.0, 2.0), (2.0,  0.0), (0.0, 0.0)];
+        let square = vec![(0.0, 0.0), (0.0, 2.0), (2.0, 2.0), (2.0, 0.0), (0.0, 0.0)];
         assert_eq!(inside(&square, &(1.0, 1.0)), true);
         assert_eq!(inside(&square, &(3.0, 3.0)), false);
 
