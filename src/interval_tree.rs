b7d86681fbd131588eec192e0c11703d0d3fc69d
Ivor Wanders <ivor@iwanders.net>
2023-11-24 20:02:34 -0500

    Eliminate the enum, format.

--- src/interval_tree.rs
+++ src/interval_tree.rs
@@ -1,30 +1,25 @@
-
 #[derive(Debug, Clone, PartialEq, Ord, PartialOrd, Eq, Copy)]
 pub struct IntervalId(pub usize);
 
 use std::num::NonZeroUsize;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 /// A node in our interval tree.
-enum Node {
-    Split {
-        /// Index to elements below of this pivot.
-        left: Option<NonZeroUsize>,
-        /// The actual center value.
-        pivot: f64,
-        /// Index to elements above or equal to this pivot.
-        right: Option<NonZeroUsize>,
+struct Node {
+    /// Index to elements below of this pivot.
+    left: Option<NonZeroUsize>,
+    /// The actual center value.
+    pivot: f64,
+    /// Index to elements above or equal to this pivot.
+    right: Option<NonZeroUsize>,
 
-        /// Count of entries in Imid.
-        count: usize,
+    /// Count of entries in Imid.
+    count: usize,
 
-        /// The next vector holds both left boundaries as well as right boundaries.
-        /// 0..count is increasing by left endpoint.
-        /// count..end is decreasing by right endpoint.
-        sorted_imid: Vec<(f64, IntervalId)>    
-    },
-    /// A placeholder, used during construction.
-    Placeholder,
+    /// The next vector holds both left boundaries as well as right boundaries.
+    /// 0..count is increasing by left endpoint.
+    /// count..end is decreasing by right endpoint.
+    sorted_imid: Vec<(f64, IntervalId)>,
 }
 
 #[derive(Debug, Clone)]
@@ -32,40 +27,55 @@ pub struct IntervalTree {
     nodes: Vec<Node>,
 }
 
-
 // Construction is very similar to a KDTree, and some of the same concepts are applicable here
 // see https://github.com/iwanders/playground-registration_icp_2d/blob/65dc9351fce30906fe7be1fadc57f95920ecd7f1/src/kdtree.rs
-
 
 impl IntervalTree {
     pub fn new(intervals: &[((f64, f64), IntervalId)]) -> Self {
         // This creation can be cleaned up significantly, currently there's a lof copying around.
 
         fn get_median(intervals: &[((f64, f64), IntervalId)]) -> f64 {
-            let mut z = intervals.iter().map(|(se, _)| [se.0, se.1]).flatten().collect::<Vec<_>>();
+            let mut z = intervals
+                .iter()
+                .map(|(se, _)| [se.0, se.1])
+                .flatten()
+                .collect::<Vec<_>>();
             z.sort_by(|&a, &b| a.partial_cmp(&b).unwrap());
             z[z.len() / 2]
         }
-        fn get_intervals(intervals: &[((f64, f64), IntervalId)], v: f64) -> Vec<((f64, f64), IntervalId)> {
-            intervals.iter().filter(|a| a.0.0 <= v && v <= a.0.1).cloned().collect()
+        fn get_intervals(
+            intervals: &[((f64, f64), IntervalId)],
+            v: f64,
+        ) -> Vec<((f64, f64), IntervalId)> {
+            intervals
+                .iter()
+                .filter(|a| a.0 .0 <= v && v <= a.0 .1)
+                .cloned()
+                .collect()
         }
-        fn get_left(intervals: &[((f64, f64), IntervalId)], v: f64) -> Vec<((f64, f64), IntervalId)> {
-            intervals.iter().filter(|a| a.0.1 < v).cloned().collect()
+        fn get_left(
+            intervals: &[((f64, f64), IntervalId)],
+            v: f64,
+        ) -> Vec<((f64, f64), IntervalId)> {
+            intervals.iter().filter(|a| a.0 .1 < v).cloned().collect()
         }
-        fn get_right(intervals: &[((f64, f64), IntervalId)], v: f64) -> Vec<((f64, f64), IntervalId)> {
-            intervals.iter().filter(|a| v < a.0.0).cloned().collect()
+        fn get_right(
+            intervals: &[((f64, f64), IntervalId)],
+            v: f64,
+        ) -> Vec<((f64, f64), IntervalId)> {
+            intervals.iter().filter(|a| v < a.0 .0).cloned().collect()
         }
 
         fn sort_imid(intervals: &[((f64, f64), IntervalId)]) -> Vec<(f64, IntervalId)> {
-            let mut left = intervals.iter().map(|z| (z.0.0, z.1)).collect::<Vec<_>>();
+            let mut left = intervals.iter().map(|z| (z.0 .0, z.1)).collect::<Vec<_>>();
             left.sort_by(|&a, &b| a.0.partial_cmp(&b.0).unwrap());
-            let mut right = intervals.iter().map(|z| (z.0.1, z.1)).collect::<Vec<_>>();
+            let mut right = intervals.iter().map(|z| (z.0 .1, z.1)).collect::<Vec<_>>();
             right.sort_by(|&a, &b| b.0.partial_cmp(&a.0).unwrap());
             left.iter().chain(right.iter()).cloned().collect()
         }
-            
+
         let mut nodes = vec![];
-    
+
         #[derive(Debug)]
         /// Helper struct for the nodes yet to be processed
         struct ProcessNode {
@@ -73,11 +83,10 @@ impl IntervalTree {
             precursor: usize,
         }
 
-
         // We use a deque, such that we can insert in the rear and pop from the front.
         // This ensures that we don't get a depth first tree.
-        nodes.push(Node::Placeholder); // push the first placeholder node
-                                       // Push the list of ondices to work on.
+        nodes.push(Node::default()); // push the first placeholder node
+                                     // Push the list of ondices to work on.
         use std::collections::VecDeque;
         let mut to_process: VecDeque<ProcessNode> = VecDeque::new();
         to_process.push_back(ProcessNode {
@@ -104,7 +113,7 @@ impl IntervalTree {
                 None
             } else {
                 let left = nodes.len();
-                nodes.push(Node::Placeholder); // left node
+                nodes.push(Node::default()); // left node
                 to_process.push_back(ProcessNode {
                     intervals: i_left,
                     precursor: left,
@@ -115,7 +124,7 @@ impl IntervalTree {
                 None
             } else {
                 let right = nodes.len();
-                nodes.push(Node::Placeholder); // left node
+                nodes.push(Node::default()); // left node
                 to_process.push_back(ProcessNode {
                     intervals: i_right,
                     precursor: right,
@@ -125,7 +134,7 @@ impl IntervalTree {
 
             // Finally, update the precursor, replacing the placeholder with a split pointing to
             // the correct nodes.
-            nodes[v.precursor] = Node::Split {
+            nodes[v.precursor] = Node {
                 pivot,
                 left,
                 right,
@@ -134,31 +143,41 @@ impl IntervalTree {
             };
         }
 
-        IntervalTree{nodes}
+        IntervalTree { nodes }
     }
 
     pub fn intervals(&self, v: f64) -> Vec<IntervalId> {
         let mut r = vec![];
 
         fn recurser(o: &mut Vec<IntervalId>, v: f64, index: usize, nodes: &[Node]) {
-            match &nodes[index] {
-                Node::Placeholder => panic!("placeholder encountered during search"),
-                Node::Split{
-                    left, pivot, right, count, sorted_imid
-                } => {
-                    if v < *pivot {
-                        // Search the left side of i mid up to left endpoint > v
-                        o.extend(sorted_imid[0..*count].iter().take_while(|(le, _)| le <= &v).map(|(_, i)| *i));
-                        if let Some(left_index) = left {
-                            recurser(o, v, left_index.get(), nodes)
-                        }
-                    } else {
-                        // Search the right side of i mid up to right endpoint < v
-                        o.extend(sorted_imid[*count..].iter().take_while(|(re, _)| re >= &v).map(|(_, i)| *i));
-                        if let Some(right_index) = right {
-                            recurser(o, v, right_index.get(), nodes)
-                        }
-                    }
+            let Node {
+                pivot,
+                left,
+                right,
+                count,
+                sorted_imid,
+            } = &nodes[index];
+            if v < *pivot {
+                // Search the left side of i mid up to left endpoint > v
+                o.extend(
+                    sorted_imid[0..*count]
+                        .iter()
+                        .take_while(|(le, _)| le <= &v)
+                        .map(|(_, i)| *i),
+                );
+                if let Some(left_index) = left {
+                    recurser(o, v, left_index.get(), nodes)
+                }
+            } else {
+                // Search the right side of i mid up to right endpoint < v
+                o.extend(
+                    sorted_imid[*count..]
+                        .iter()
+                        .take_while(|(re, _)| re >= &v)
+                        .map(|(_, i)| *i),
+                );
+                if let Some(right_index) = right {
+                    recurser(o, v, right_index.get(), nodes)
                 }
             }
         }
@@ -174,13 +193,19 @@ mod test {
     use super::*;
 
     fn get_interval_ids(intervals: &[((f64, f64), IntervalId)], v: f64) -> Vec<IntervalId> {
-        intervals.iter().filter_map(|((s, e), i)| {
-            if *s <= v && v <= *e {
-                Some(i)
-            } else {
-                None
-            }
-        }).cloned().collect()
+        intervals
+            .iter()
+            .filter_map(
+                |((s, e), i)| {
+                    if *s <= v && v <= *e {
+                        Some(i)
+                    } else {
+                        None
+                    }
+                },
+            )
+            .cloned()
+            .collect()
     }
 
     fn assert_intervals(a: &[IntervalId], b: &[IntervalId]) {
